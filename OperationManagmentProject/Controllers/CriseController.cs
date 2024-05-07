6a8c7278a592141a970f387bce764b3de9181de0
mousamajdi1 <m.majdi@quant.sa>
2024-05-07 22:15:14 +0300

    fix action name

--- OperationManagmentProject/Controllers/CriseController.cs
+++ OperationManagmentProject/Controllers/CriseController.cs
@@ -236,7 +236,7 @@ namespace OperationManagmentProject.Controllers
                     Id = action.Id,
                     CriseId = action.CriseId,
                     ActionId = action.ActionId,
-                    ActionName = actionNames.ContainsKey(action.ActionId) ? actionNames[action.Id] : "",
+                    ActionName = actionNames.ContainsKey(action.ActionId) ? actionNames[action.ActionId] : "",
                     Report = action.Report,
                     CreatedAt = action.CreatedAt,
                     UpdatedAt = action.UpdatedAt,
